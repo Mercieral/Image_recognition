f524571d964608adf77dc66258197b20701d66b2
Aaron Mercier <mercieal@rose-hulman.edu>
2017-01-13 18:05:48 -0600

    prealloc features matrix and call featureExtractor/normalize
    prealloc features matrix and call featureExtractor/normalize

--- SunsetDetector/sunsetDetector.m
+++ SunsetDetector/sunsetDetector.m
@@ -8,14 +8,28 @@ subdir6 = 'sunsetDetectorImages/TestDifficultNonsunsets';
 subdirs = {subdir1, subdir2, subdir3, subdir4, subdir5, subdir6}; 
 fileLists = {dir(subdir1), dir(subdir2), dir(subdir3), dir(subdir4), dir(subdir5), dir(subdir6)};
 
+% preallocate memory to prevent inefficient reallocation at each iteration.
+% 10000 is assumed to be the maximum, and it will shrink the array to the
+% exact size at the end (suggested by MATLAB spec)
+features = zeros(10000, 294);
+
 count = 0;
 for i = 1:size(fileLists, 2)
     for j = 3:size(fileLists{i})
-        %img = imread([subdir i '/'  fileLists{i}(j).name]);
-        count = count + 1;
-        % TODO: insert code of function call here to operate on image.
-        % Hint: debug the loop body on 1-2 images BEFORE looping over lots of them...
+        %if count < 10
+            count = count + 1;
+            img = imread([subdirs{i} '/'  fileLists{i}(j).name]);
+            extractedFeatures = featureExtractor(img);
+            features(count,:) = extractedFeatures(:,1);
+        %end
     end
 end
-count
+
+features = features(1:count, 1:294);
+size(features)
+
+norm = normalizeFeatures01(features);
+
+% Train svm with only the training sets from norm
+% TODO
 
