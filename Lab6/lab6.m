05341be91b4bcabe1382925de395dff23fe4c3c4
Larry Gates <gateslm@rose-hulman.edu>
2017-01-23 20:33:52 -0500

    Up to the point of assigning colors

--- Lab6/lab6.m
+++ Lab6/lab6.m
@@ -1,28 +1,50 @@
-function result = lab6(img)
-    k = 5;
-    seed = 0;
+function results = lab6(img, k, seed, iterations)
+%     k = 5;
+%     seed = 0;
     rand('state', seed);
     % Initialize k-cluster means, 3-D point
-    means = rand(k, 3);
+    means = rand(k, 3)
     
-    result = zeros(size(img, 1), size(img,2), size(img, 3));
+    result = zeros(size(img, 1), size(img,2));
     
-    for r = 1:size(img, 1)
-        for c = 1:size(img,2)
-            minMean = size(img,1) * size(img, 2);
-            minK = 0;
-            red = double(img(r, c, 1))/255;
-            green = double(img(r, c, 2))/255;
-            blue = double(img(r, c, 3))/255;
-            for i = 1:k
-                d = euclideanDist(red, green, blue, means(i, 1), means(i, 2), means(i, 3));
-                if d < minMean
-                    minMean = d;
-                    minK = i;
+    for iter = 1:iterations
+        for r = 1:size(img, 1)
+            for c = 1:size(img,2)
+                % Going through and finding distance from each cluster
+                minMean = size(img,1) * size(img, 2);
+                minK = 0;
+                red = double(img(r, c, 1))/255;
+                green = double(img(r, c, 2))/255;
+                blue = double(img(r, c, 3))/255;
+                for i = 1:k
+                    d = euclideanDist(red, green, blue, means(i, 1), means(i, 2), means(i, 3));
+                    if d < minMean
+                        minMean = d;
+                        minK = i;
+                    end
                 end
+                result(r, c) = minK;
             end
-            result(r, c, 1) = minK;
         end
+        % Re-compute the mean of all clusters
+        for i = 1:k
+            % Handling cluster with zero pixels
+            if size(find(result(:,:)==i),1) == 0
+                continue;
+            else 
+                [x, y] = find(result(:,:)==i);
+                means(i,1) = mean2(img(x, y, 1))/255;
+                means(i,2) = mean2(img(x, y, 2))/255;
+                means(i,3) = mean2(img(x, y, 3))/255;
+            end
+        end
+    end 
+    % Add color
+    results = zeros(size(img, 1), size(img,2), size(img, 3));
+    for i = 1:k
+%         results(find(result==i),1) = means(i,1);
+%         results(find(result==i),2) = means(i,2);
+%         results(find(result==i),3) = means(i,3);
     end
 
 end 
