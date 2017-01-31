153b7a187470eee003fa8c6a3410765dfd029edb
Aaron Mercier <mercieal@rose-hulman.edu>
2017-01-30 21:17:26 -0500

    Correct image extensions
    Correct image extensions

--- Lab7/PCA.m
+++ Lab7/PCA.m
@@ -32,27 +32,6 @@ v1Norm = normalize(v1);
 v2Norm = normalize(v2);
 v3Norm = normalize(v3);
 
-% img1 = imread([subdir '/20060606_173500.jpg']);
-% img2 = imread([subdir '/20060607_081800.jpg']);
-% 
-% syms c1 c2 c3
-% eqn1 = img1(1,1,1) == mean2(1,1,1) + (c1 * v1(1,1,1)) + (c2 * v2(1,1,1)) + (c3 * v3(1,1,1));
-% eqn2 = img1(1,1,2) == mean2(1,1,2) + (c1 * v1(1,1,2)) + (c2 * v2(1,1,2)) + (c3 * v3(1,1,2));
-% eqn3 = img1(1,1,3) == mean2(1,1,3) + (c1 * v1(1,1,3)) + (c2 * v2(1,1,3)) + (c3 * v3(1,1,3));
-% [A,B] = equationsToMatrix([eqn1, eqn2, eqn3], [c1, c2, c3]);
-% X = linsolve(A,B)
-% 
-% resImg1 = normalize(mean2 + (double(X(1)) .* v1) + (double(X(2)) .* v2) + (double(X(3)) .* v3));
-% 
-% syms c4 c5 c6
-% eqn4 = img2(1,1,1) == mean2(1,1,1) + (c4 * v1(1,1,1)) + (c5 * v2(1,1,1)) + (c6 * v3(1,1,1));
-% eqn5 = img2(1,1,2) == mean2(1,1,2) + (c4 * v1(1,1,2)) + (c5 * v2(1,1,2)) + (c6 * v3(1,1,2));
-% eqn6 = img2(1,1,3) == mean2(1,1,3) + (c4 * v1(1,1,3)) + (c5 * v2(1,1,3)) + (c6 * v3(1,1,3));
-% [A2,B2] = equationsToMatrix([eqn4, eqn5, eqn6], [c4, c5, c6]);
-% X2 = linsolve(A2,B2)
-% 
-% resImg2 = normalize(mean2 + (double(X2(1)) .* v1) + (double(X2(2)) .* v2) + (double(X2(3)) .* v3));
-% solve(img1 = mean2 + (c1 * v1) + (c2 * v2) + (c3 * v3))
 C = zeros(size(fileList,1),3);
 
 for i = 3:size(fileList,1)
@@ -76,3 +55,5 @@ for i = 3:size(fileList,1)
     C(i,3) = double(X(3));
     
 end
+
+plot(1:size(fileList,1), C(:,1))
